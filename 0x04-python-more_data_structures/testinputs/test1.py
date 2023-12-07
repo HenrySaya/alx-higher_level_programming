82b6ecf34d36418f3e7ec98baba2d5a8b48ada1f
Henry Saya <henrysaya@skiff.com>
2023-12-07 10:23:53 -0500

    Create File 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/testinputs/test1.py
+++ 0x04-python-more_data_structures/testinputs/test1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from ..0-square_matrix_simple import square_matrix_simple
+from /home/dev_henry/alx-higher_level_programming/0x04-python-more_data_structures/0-square_matrix_simple import square_matrix_simple
 
 matrix = [
     [1, 2, 3],
