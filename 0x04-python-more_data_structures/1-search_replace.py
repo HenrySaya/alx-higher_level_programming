6fd216af5bb1e7dc11fc2f8ea93a3a01b9d71697
Henry Saya <henrysaya@skiff.com>
2023-12-07 11:00:18 -0500

    Debug 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    new_list = []
     for i in my_list:
-        new_row = list(map(lambda i: replace if i == search else i, my_list))
-    return new_row
+        new_list = list(map(lambda i: replace if i == search else i, my_list))
+    return new_list
