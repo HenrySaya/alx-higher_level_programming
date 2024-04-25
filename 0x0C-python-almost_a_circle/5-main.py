ccbbb773a9ef5f80450ec555ee6a96b2185e810b
Henry Saya <henrysaya@skiff.com>
2024-04-25 20:48:13 +0300

    5-main.py Display #0

---
+++ 0x0C-python-almost_a_circle/5-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+5-main
+"""
+from models.rectangle import Rectangle
+
+
+if __name__ == "__main__":
+
+    r1 = Rectangle(4, 6)
+    r1.display()
+
+    print("---")
+
+    r2 = Rectangle(2,3)
+    r2.display()
